ec828becaeaff212bb7f0db3d25e54e9245226db
cyotee <cyotee@gmail.com>
2016-03-15 15:03:42 -0500

    Added Docker build command to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 ./install.sh
+
+DOCKER_REGISTRY=docker.io
+
+DOCKER_REGISTRY_NAMESPACE=cyotee
+
+DOCKER_IMAGE_NAME=jbpm
+
+DOCKER_IMAGE_VERSION=latest
+
+echo "Building Docker Image with command docker build -t ${DOCKER_REGISTRY}/${DOCKER_REGISTRY_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} ."
+docker build --force-rm=true --rm=true -t ${DOCKER_REGISTRY}/${DOCKER_REGISTRY_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
+echo =========================================================================
+echo Docker image is ready.  Try it out by running:
+echo     docker run --rm -ti -P ${DOCKER_IMAGE_NAME}
+echo =========================================================================
